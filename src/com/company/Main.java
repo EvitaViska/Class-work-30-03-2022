968d1fe15a37c4494c1f57ed6a34f232415b0fd6
Evita Viska <viskaevita@gmail.com>
2022-03-30 19:50:36 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,127 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+        // Task1
+        Scanner scanner = new Scanner(System.in);
+
+        //Read number 1
+        System.out.println("Input the first number: ");
+        int number1 = scanner.nextInt();
+
+        //Read number 2
+        System.out.println("Input second number: ");
+        int number2 = scanner.nextInt();
+
+        //Read number 3
+        System.out.println("Input third number: ");
+        int number3 = scanner.nextInt();
+
+        int smallest;
+
+        if (number1 < number2){
+            smallest = number1;
+        }else{
+            smallest = number2;
+        }
+        if (number3 < smallest){
+            smallest = number3;
+        }
+        System.out.println("The smallest number is: " + smallest);
+*/
+        /*
+        //Task2
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Percentage obtained by a student: ");
+        int percentage = scanner.nextInt();
+
+        char gradeA = 'A';
+        char gradeB = 'B';
+        char gradeC = 'C';
+        char gradeD = 'D';
+
+        if (percentage >= 90) {
+            System.out.println("Grade is: " + gradeA);
+        } else if (percentage >= 75 && 90 > percentage) {
+            System.out.println("Grade is: " + gradeB);
+        } else if (percentage >= 65 && 75 > percentage) {
+            System.out.println("Grade is: " + gradeC);
+        } else {
+            System.out.println("Grade is: " + gradeD);
+        }
+    } */
+        /*
+        //Task2 David
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Percentage obtained by a student: ");
+        int percentage = scanner.nextInt();
+
+        char grade;
+
+        if (percentage > 90) {
+            grade = 'A';
+        } else if (percentage > 75) {
+            grade = 'B';
+        } else if (percentage > 65) {
+            grade = 'C';
+        } else {
+            grade = 'D';
+        }
+
+        //Print grade
+        System.out.println("The students grade is: " + grade);
+    }*/
+
+        /**
+         Pseudo Code for task 3:
+
+         Read temperature
+
+         If temperature >= 80
+            activity = swimming
+         Else if temperature >= 60 && temperature < 80
+            activity = tennis
+         Else if temperature >= 40 && temperature < 60
+            activity = golf
+         Else
+            activity = skiing
+
+         Print activity
+
+         */
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please input temperature: ");
+        int temperature = scanner.nextInt();
+
+        String activity;
+
+        if (temperature >= 80 && temperature <= 95){
+            activity = "Swimming";
+        } else if (60 <= temperature && temperature < 80){
+            activity = "Tennis";
+        } else if (40 <= temperature && temperature < 60){
+            activity = "Golf";
+        } else if (temperature > 95 || temperature < 20){
+            activity = "Visit our shops!";
+        } else if (temperature < 40 && temperature >= 20){
+            activity = "Skiing";
+        } else {
+            return;
+        }
+
+        //Print activity
+        System.out.println("Appropriate activity: " + activity);
+    }
+
+
+
+}
